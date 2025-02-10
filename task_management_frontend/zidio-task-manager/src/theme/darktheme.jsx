2cbea523e4afd3a19c128c0078703eef573a2de1
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2025-02-10 14:01:28 +0530

    initial

---
+++ task_management_frontend/zidio-task-manager/src/theme/darktheme.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const darkTheme=createTheme({
+    palette:{
+        mode:'dark',
+        background:{
+            default:'#0c071b'
+        },
+        text:{
+            primary:'#fff'
+        },
+        primary:{
+            
+        }
+    }
+})
