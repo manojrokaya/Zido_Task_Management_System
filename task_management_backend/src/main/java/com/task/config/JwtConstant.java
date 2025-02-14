fa34fbb97f44dca63189fe3f1a10c143317ef07c
manojrokaya <manojrokaya0876@gmail.com>
2025-02-14 19:40:54 +0530

    change jwt token

--- task_management_backend/src/main/java/com/task/config/JwtConstant.java
+++ task_management_backend/src/main/java/com/task/config/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.task.config;
 
 public class JwtConstant {
-	public static final String SECRET_KEY="fjslwnsomwoslwolsposapwjmpslspwpspswldidlwoslwpspswwsspwlsp";
+	public static final String SECRET_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ";
 	public static  final String JWT_HEADER="Authorization";
 
 }
